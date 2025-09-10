6e73685f698b17862d4365a28ac431f30b5a38e8
Afmano <86706321+Afmano@users.noreply.github.com>
2025-09-10 14:13:10 +0300

    Added telegram bot api integration 2/2

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -6,12 +6,14 @@ interface Config {
   port: number;
   nodeEnv: string;
   connectionString: string | undefined;
+  botToken: string | undefined;
 }
 
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
   nodeEnv: process.env.NODE_ENV || 'development',
   connectionString: process.env.CONNECTION_STRING,
+  botToken: process.env.BOT_TOKEN,
 };
 
 export default config;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,9 @@
 import app from './app.ts';
 import config from './config/config.ts';
+import { startBot } from './telegram/bot.ts';
 
 app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`);
 });
+
+startBot();
