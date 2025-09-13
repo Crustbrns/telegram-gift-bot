dedca00c7c6311ed0fef15c2cb13465be80e1daa
Afmano <86706321+Afmano@users.noreply.github.com>
2025-09-14 02:54:29 +0300

    Host removed for Egor at 3am UTC+2.

--- server/src/swagger.ts
+++ server/src/swagger.ts
@@ -1,12 +1,10 @@
 import swaggerAutogen from 'swagger-autogen';
-import config from './config/config.ts';
 
 const doc = {
   info: {
     title: 'GIFT BOT API',
     description: 'API for gift bot'
   },
-  host: config.host
 };
 
 const swaggerFile = './swagger-output.json';
