3759ddbf13c462e35cba3a30d6232b3d4b743ea8
Afmano <86706321+Afmano@users.noreply.github.com>
2025-09-15 15:45:25 +0300

    Host returned.

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -4,6 +4,7 @@ dotenv.config();
 
 interface Config {
   port: number;
+  host:string;
   nodeEnv: string;
   connectionString: string | undefined;
   botToken: string | undefined;
@@ -13,6 +14,7 @@ interface Config {
 
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
+  host: process.env.HOST || "https://telegram-gift-bot-3jp7.onrender.com",
   nodeEnv: process.env.NODE_ENV || 'development',
   connectionString: process.env.CONNECTION_STRING,
   botToken: process.env.BOT_TOKEN,
--- server/src/swagger.ts
+++ server/src/swagger.ts
@@ -1,10 +1,12 @@
 import swaggerAutogen from 'swagger-autogen';
+import config from './config/config.js';
 
 const doc = {
   info: {
     title: 'GIFT BOT API',
-    description: 'API for gift bot'
+    description: 'API for gift bot',
   },
+  host: config.host,
 };
 
 const swaggerFile = './swagger-output.json';
