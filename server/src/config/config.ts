2ec738117a942046e6bd34c222114d544f8b9a70
Afmano <86706321+Afmano@users.noreply.github.com>
2025-09-18 14:35:46 +0300

    Ping URL changed.

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -4,22 +4,24 @@ dotenv.config();
 
 interface Config {
   port: number;
-  host:string;
+  host: string;
   nodeEnv: string;
   connectionString: string | undefined;
   botToken: string | undefined;
   secretPassword: string;
   RTP: number;
+  pingURL: string;
 }
 
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
-  host: process.env.HOST || "telegram-gift-bot-3jp7.onrender.com",
+  host: process.env.HOST || 'telegram-gift-bot-3jp7.onrender.com',
   nodeEnv: process.env.NODE_ENV || 'development',
   connectionString: process.env.CONNECTION_STRING,
   botToken: process.env.BOT_TOKEN,
   secretPassword: process.env.SECRET_PASSWORD || 'Egorprivet123',
   RTP: Number(process.env.RTP) || 0.75,
+  pingURL: process.env.PING_URL || '',
 };
 
 export default config;
--- server/src/routes/pingRoute.ts
+++ server/src/routes/pingRoute.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import type { Request, Response, NextFunction } from 'express';
+import config from '../config/config.js';
 
 const router = Router();
 
 router.get('/', async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const pingURL = req.host + '/ping';
+    const pingURL = config.pingURL;
     await fetch(pingURL);
     console.log('Sended ping to ' + pingURL);
     res.status(200);
