717447f6242b9a530c81065b1349814e282d0a12
Afmano <86706321+Afmano@users.noreply.github.com>
2025-09-19 11:59:21 +0300

    Enabled cors.

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,7 +5,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import config from './config/config.js';
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from './swagger-output.json' with { type: 'json' };
-//import cors from 'cors';
+import cors from 'cors';
 // import slowDown from 'express-slow-down';
 import morgan from "morgan";
 import pingRoute from './routes/pingRoute.js';
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 // app.use(helmet());
-//app.use(cors());//change origin in future
+app.use(cors());//change origin in future
 
 // const limiter = slowDown({
 //   windowMs: 15 * 60 * 1000, // 15 minutes
