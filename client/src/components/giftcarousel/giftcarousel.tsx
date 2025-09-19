e889008d472a5925d79d428bc219fec058d6caec
Crustbrns <crustbrns@gmail.com>
2025-09-19 10:38:33 +0200

    Button implemented

--- client/src/components/giftcarousel/giftcarousel.tsx
+++ client/src/components/giftcarousel/giftcarousel.tsx
@@ -35,59 +35,52 @@ function GenerateGift(gifts: Array<string>, count: number): Array<GiftImage> {
 }
 
 function Giftcarousel({ gifts }: Props) {
-  const [cards] = React.useState<GiftImage[]>(() => GenerateGift(gifts, 14));
+  const [cards, setCards] = React.useState<GiftImage[]>(() =>
+    GenerateGift(gifts, 14)
+  );
   const [offset, setOffset] = React.useState(0);
   const [width, setWidth] = React.useState(0);
+  const speedRef = React.useRef(0);
+  const [isInactive, setIsInactive] = React.useState(false);
+
+  function Roll() {
+    if(!isInactive) {
+      setCards(GenerateGift(gifts, 14));
+      setOffset(0);
+      setIsInactive(true);
+      speedRef.current = 58.4;
+      setTimeout(() => {
+        setIsInactive(false);
+      }, 900);
+    }
+  }
 
   useEffect(() => {
     let frame: number;
     let lastTime = performance.now();
-    // const startTime = lastTime;
     function checkWidth() {
       setWidth(window.innerWidth);
     }
-
-    // проверяем сразу
     checkWidth();
 
-    // навешиваем слушатель
     window.onresize = checkWidth;
+    // const speed = { current: 58.4 };
 
-    const speed = { current: 58.4 };
-    // const accel = { current: 0.05 };
-
+    // setIsInactive(true);
+    // else setIsInactive(false);
     function animate(now: number) {
       const delta = (now - lastTime) / (1000 / 60);
       lastTime = now;
-
-      setOffset((prev) => prev - speed.current * delta);
-      // setCount((prev) => prev + Math.max(1, Math.floor(delta)/100));
-      speed.current = Math.max(0, speed.current - 1 * delta);
-      // if (now - startTime < 400) {
-      //   speed.current += 3;
-      // } else {
-      //   if (now - startTime < 2600) {
-      //     speed.current = Math.max(0, speed.current - 0.6);
-      //   } else {
-      //     speed.current = Math.max(0, speed.current - accel.current);
-      //     accel.current += 0.00025;
-      //   }
-      // }
-
-      // console.log(count);
+      setOffset((prev) => prev - speedRef.current * delta);
+      // setOffset((prev) => prev - speed.current * delta);
+      speedRef.current = Math.max(0, speedRef.current - 1 * delta);
       frame = requestAnimationFrame(animate);
     }
 
     frame = requestAnimationFrame(animate);
-    // const interval = setInterval(() => {
-    //   console.log("Тик" + count, new Date().toLocaleTimeString());
-    //   setCount((prev) => prev + 1);
-    // }, 1000);
 
-    // очищаем интервал при размонтировании
     return () => {
       cancelAnimationFrame(frame);
-      // clearInterval(interval);
     };
   }, []);
 
@@ -105,7 +98,7 @@ function Giftcarousel({ gifts }: Props) {
               transform: `translateX(${offset}px)`,
               display: "flex",
               gap: "10px",
-              marginLeft: width > 720 ? 0 : -(720 - width)/2
+              marginLeft: width > 720 ? 0 : -(720 - width) / 2,
             }}
           >
             {cards.map((gift, i) => (
@@ -117,7 +110,14 @@ function Giftcarousel({ gifts }: Props) {
       {/* <div className={classes.arrow_container}>
         <TbTriangleFilled className={classes.arrow} color="black" size={25} />
       </div> */}
-      <div className={classes.button}>Мне повезет!</div>
+      <div
+        className={`${classes.button} ${
+          isInactive ? classes.button_inactive : ""
+        }`}
+        onClick={Roll}
+      >
+        Мне повезет!
+      </div>
     </div>
   );
 }
